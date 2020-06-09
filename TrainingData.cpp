1e5b0db41dc6578d8a3102052937b4417e30050c
Edan Toledo <TLDEDA001@myuct.ac.za>
2020-06-09 16:28:28 +0200

    Added setter method and a method that loads the data from a text file

--- TrainingData.cpp
+++ TrainingData.cpp
@@ -19,3 +19,36 @@ const std::vector<float> TLDEDA001::TrainingData::getInputs(const int index) con
 
     return returnvec;
 }
+
+
+void TLDEDA001::TrainingData::AddDataSet(const float input1, const float input2, const float output)
+{
+    this->input1.push_back(input1);
+    this->input2.push_back(input2);
+    this->output.push_back(output);
+}
+
+
+void TLDEDA001::TrainingData::clearData()
+{
+    this->input1.clear();
+    this->input2.clear();
+    this->output.clear();
+}
+
+//load data from training
+void TLDEDA001::TrainingData::LoadDataFromFile(std::string textfile)
+{
+    std::ifstream inp(textfile);
+    std::string line;
+
+    while (getline(inp, line))
+    {
+        std::stringstream s(line);
+        float in1, in2, out;
+        s >> in1 >> std::ws >> in2 >> std::ws >> out;
+        input1.push_back(in1);
+        input2.push_back(in2);
+        output.push_back(out);
+    }
+}
